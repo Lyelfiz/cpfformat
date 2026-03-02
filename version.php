50f8b81dfe94ce98da996df16e4b81d3ec739c88
Lyelfiz <Lyelfiz@gmail.com>
2026-03-02 09:34:08 -0400

    Update version.php

--- version.php
+++ version.php
@@ -4,8 +4,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026021000;
+$plugin->version = 2026030200;
 $plugin->requires = 2024041900;
 $plugin->component = 'local_cpfformat';
-$plugin->release = '3.0.1';
+$plugin->release = '3.0.2';
 $plugin->maturity = MATURITY_STABLE;
